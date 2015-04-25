c9fb6cf7a4ffa693df88e8da85d145bcf96a6de5
Corey Powell <mistdragon100@gmail.com>
2015-04-25 00:14:17 -0500

    Docs for SoundSource and SoundStream

--- docstub/sound_source.rb
+++ docstub/sound_source.rb
@@ -1,4 +1,85 @@
 module SFML
   class SoundSource < AlResource
+    # Whether the sound is stopped, paused or currently playing.
+    module Status
+      # @return [Integer]
+      Stopped = 0
+      # @return [Integer]
+      Paused  = 1
+      # @return [Integer]
+      Playing = 2
+    end
+
+    # @param [Float] pitch
+    # @return [self]
+    def set_pitch(pitch)
+    end
+
+    # @param [Float] volume
+    # @return [self]
+    def set_volume(volume)
+    end
+
+    # @overload set_position(position)
+    #   @param [Vector3f] position
+    # @overload set_position(x, y, z)
+    #   @param [Float] x
+    #   @param [Float] y
+    #   @param [Float] z
+    # @return [self]
+    def set_position(*args)
+    end
+
+    # @param [Boolean] bool
+    # @return [self]
+    def set_relative_to_listener(bool)
+    end
+
+    # @param [Float] distance
+    # @return [self]
+    def set_min_distance(distance)
+    end
+
+    # @param [Float] attenuation
+    # @return [self]
+    def set_attenuation(attenuation)
+    end
+
+    # @return [Float]
+    def get_pitch
+    end
+
+    # @return [Float]
+    def get_volume
+    end
+
+    # @return [Vector3f]
+    def get_position
+    end
+
+    # @return [Boolean]
+    def is_relative_to_listener
+    end
+
+    # @return [Float]
+    def get_min_distance
+    end
+
+    # @return [Float]
+    def get_attenuation
+    end
+
+    alias :pitch= :set_pitch
+    alias :volume= :set_volume
+    alias :position= :set_position
+    alias :relative_to_listener= :set_relative_to_listener
+    alias :min_distance= :set_min_distance
+    alias :attenuation= :set_attenuation
+    alias :pitch :get_pitch
+    alias :volume :get_volume
+    alias :position :get_position
+    alias :relative_to_listener? :is_relative_to_listener
+    alias :min_distance :get_min_distance
+    alias :attenuation :get_attenuation
   end
 end
--- docstub/sound_stream.rb
+++ docstub/sound_stream.rb
@@ -1,4 +1,53 @@
 module SFML
   class SoundStream < SoundSource
+    # @return [self]
+    def play
+    end
+
+    # @return [self]
+    def pause
+    end
+
+    # @return [self]
+    def stop
+    end
+
+    # @return [Integer]
+    def get_channel_count
+    end
+
+    # @return [Integer]
+    def get_sample_rate
+    end
+
+    # @return [SoundSource::Status]
+    def get_status
+    end
+
+    # @param [SFML::Time] time
+    # @return [self]
+    def set_playing_offset(time)
+    end
+
+    # @return [SFMLTime]
+    def get_playing_offset
+    end
+
+    # @param [Boolean] bool
+    # @return [self]
+    def set_loop(bool)
+    end
+
+    # @return [Boolean]
+    def get_loop
+    end
+
+    alias :channel_count :get_channel_count
+    alias :sample_rate :get_sample_rate
+    alias :status :get_status
+    alias :playing_offset= :set_playing_offset
+    alias :playing_offset :get_playing_offset
+    alias :loop= :set_loop
+    alias :loop? :get_loop
   end
 end
